55fe06ea53279865eab3d80b320633d20beffa5d
Sahil Gupta <guptasahil541@gmail.com>
2021-11-02 15:26:49 +0530

    added a route and controller for posts

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/users_controller');
+const postsController = require('../controllers/posts_controller');
 
 router.get('/profile', userController.profile);
+router.get('/posts', postsController.posts);
 
 module.exports = router;
